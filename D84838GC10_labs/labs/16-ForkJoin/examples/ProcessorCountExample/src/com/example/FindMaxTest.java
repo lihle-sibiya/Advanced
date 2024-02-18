2dc12a3e4a007cfae48d03ab4c07956cebb7cd3d
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-02-18 23:51:04 +0200

    15th commit

--- D84838GC10_labs/labs/16-ForkJoin/examples/ProcessorCountExample/src/com/example/FindMaxTest.java
+++ D84838GC10_labs/labs/16-ForkJoin/examples/ProcessorCountExample/src/com/example/FindMaxTest.java
@@ -1,2 +1,24 @@
-package com.example;public class FindMaxTest {
+package com.example;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class FindMaxTest {
+    public static void main(String[] args) {
+        int[] data = new int[1024 * 1024 * 128]; //512MB
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = ThreadLocalRandom.current().nextInt();
+        }
+
+        ///////////////
+        int max = Integer.MIN_VALUE;
+        for (int value : data) {
+            if (value > max) {
+                max = value;
+                System.out.println(max + " ");
+
+            }
+        }
+        System.out.println("Max value found:" + max);
+    }
 }
